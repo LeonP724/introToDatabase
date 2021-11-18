3d7f0b6f278dc3e5c6eff42821ab574c537ad42e
Ziga Benko <zigac.benko@gmail.com>
2021-11-18 11:16:07 +0100

    Add missing library

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from sqla_wrapper import SQLAlchemy
-
+import os
 
 app = Flask(__name__)
 # db = SQLAlchemy("sqlite:///db.sqlite")
